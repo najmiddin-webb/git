537977be2823905018626bd1acdc0ecdd132b91d
Najmiddin <main@gmail.com>
2024-02-27 18:59:42 +0500

    fds

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Image from "next/image";
 import Logo from "../public/Remove-bg.ai_1701116393719 (1).png";
 import { CiWarning } from "react-icons/ci";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +18,28 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="main_div">
-          <div className="max-w-7xl mx-auto p-4 ">
+          <div className="max-w-7xl mx-auto p-4">
             {/* <Navbar /> */}
             <div className="flex items-end justify-center"></div>
-            <h1 className="text-[16px] mb-10 md:text-xl text-white text-center rounded-xl border-red-600">
+            <h1 className="text-[16px]  md:text-xl text-white text-center rounded-xl border-red-600">
               <center>
                 <CiWarning className="text-5xl text-red-500 mb-2 animation" />
+                <h1 className="text-2xl">DIQQAT</h1>
               </center>
               Taqiqlangan diniy mazmundagi materiallarni tayyorlash, saqlash,
               tarqatish va namoyish etish Jinoyat kodeksining 244¹-moddasiga
               asosan <b>5</b> yildan <b>8 </b>yilgacha ozodlikdan maxrum qilish
               bilan jazolanadi
             </h1>
-            {children}
+            <center>
+              <Link
+                href={"https://lex.uz/uz/ "}
+                className="text-white text-2xl"
+              >
+                https://lex.uz/uz/{" "}
+              </Link>
+            </center>
+            <div className="mt-5">{children}</div>
           </div>
         </div>
       </body>
