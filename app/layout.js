80913c9c5893e2d9f647881a8c6c8cc93f799961
Najmiddin <main@gmail.com>
2024-02-29 20:34:19 +0500

    fsd

--- app/layout.js
+++ app/layout.js
@@ -21,24 +21,23 @@ export default function RootLayout({ children }) {
           <div className="max-w-7xl mx-auto p-4">
             {/* <Navbar /> */}
             <div className="flex items-end justify-center"></div>
-            <h1 className="text-[16px]  md:text-xl text-white text-center rounded-xl border-red-600">
+            <h1 className="text-[15px]  md:text-xl text-white text-center rounded-xl border-red-600">
               <center>
                 <CiWarning className="text-5xl text-red-500 mb-2 animation" />
                 <h1 className="text-2xl">DIQQAT</h1>
               </center>
               Taqiqlangan diniy mazmundagi materiallarni tayyorlash, saqlash,
-              tarqatish va namoyish etish Jinoyat kodeksining 244¹-moddasiga
+              tarqatish va namoyish etish Jinoyat kodeksining{" "}
+              <Link
+                href={"https://lex.uz/uz/ "}
+                className="text-blue-600 underline"
+              >
+                244¹-moddasiga {""}
+              </Link>
               asosan <b>5</b> yildan <b>8 </b>yilgacha ozodlikdan maxrum qilish
               bilan jazolanadi
             </h1>
-            <center>
-              <Link
-                href={"https://lex.uz/uz/ "}
-                className="text-white text-2xl"
-              >
-                https://lex.uz/uz/{" "}
-              </Link>
-            </center>
+            <center></center>
             <div className="mt-5">{children}</div>
             <div className="flex items-center justify-center mt-20">
               <Link
