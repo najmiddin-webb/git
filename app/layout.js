7bd48568aac1f76d556bb00c3101c52420d98fb7
najmiddin <najmiddin@gmail.com>
2024-05-20 10:24:30 +0500

    fs

--- app/layout.js
+++ app/layout.js
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
               </Link>
             </div>
           <div>
-            <h1 className='text-white text-center flex justify-center mt-6 md:mt-60 md:text-2xl text-[15px]'>
+            <h1 className='text-white text-center flex justify-center mt-6 md:mt-52 md:text-2xl text-[15px]'>
                   Bu turdagi taqiqlangan materiallar boshqa ijtimoiy internet tarmoqlarida ham tarqatilgan bo`lishi mumkin.
             </h1>
           </div>
